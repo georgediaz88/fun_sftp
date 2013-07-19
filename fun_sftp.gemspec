54c0e5072b5eba558d870ea05f7db12363763882
George Diaz <georgediaz88@yahoo.com>
2013-07-18 22:52:45 -0500

    Add FunSftp Specs Yay!

--- fun_sftp.gemspec
+++ fun_sftp.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
   s.add_dependency 'net-sftp'
   s.add_development_dependency 'rspec'
+  s.add_development_dependency 'pry'
 end
---
+++ spec/fun_sftp/sftp_client_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+module FunSftp
+  describe 'sftp client' do
+
+    let(:sftp_mock) {double('sftp')} #sftp_mock is the @client var
+
+    before do
+      Net::SFTP.stub(:start => sftp_mock)
+    end
+
+    context 'initialization' do
+      it 'should initialize with creds' do
+        expect(SFTPClient).to receive(:new).with('localhost', 'user1', 'pass')
+        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
+      end
+    end
+
+    context 'file utilities' do
+      it "should return entries without prepended '.' " do
+        dir1 = double("sftp_dir", :name => ".")
+        dir2 = double("sftp_dir", :name => "import_docs")
+        dir3 = double("sftp_dir", :name => "training_docs")
+        sftp_mock.stub_chain(:dir, :entries).with('Desktop').and_return([dir1, dir2, dir3])
+        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
+        expect(@sftp_cli.entries('Desktop')).to eq([dir2.name, dir3.name])
+      end
+
+      it 'should read both lines from test file' do
+        file = File.expand_path('../../support/test1.txt', __FILE__)
+        sftp_mock.stub_chain(:file, :open).with(file).and_return(File.open(file))
+        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
+        expect(@sftp_cli).to receive(:puts).twice
+        @sftp_cli.read(file)
+      end
+
+      it 'should return false for directory not found' do
+        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
+        @sftp_cli.stub(:entries).and_raise('Net::SFTP::StatusException')
+        expect(@sftp_cli.has_directory?('bogus_directory')).to eql(false)
+      end
+    end
+
+  end
+end

336711a3d97d44ec9c25d99d2ccf05db4b059fb7
George Diaz <georgediaz88@yahoo.com>
2014-10-25 21:36:18 -0500

    Changes to chdir method Cleanup Method Upgrade to Latest RSpec Bump Version Update Syntax Preserve source declarations Shift Specs Final Tidyups

--- spec/fun_sftp/sftp_client_spec.rb
+++ spec/fun_sftp/sftp_client_spec.rb
@@ -3,86 +3,101 @@ require 'spec_helper'
 module FunSftp
   describe 'sftp client' do
 
-    let(:sftp_mock) {double('sftp')} #sftp_mock is the @client var
+    let(:sftp_mock) { double('sftp') }
 
     before do
-      Net::SFTP.stub(:start => sftp_mock)
+      allow(Net::SFTP).to receive(:start).and_return(sftp_mock)
     end
 
     context 'initialization' do
       it 'should initialize with creds' do
         expect(SFTPClient).to receive(:new).with('localhost', 'user1', 'pass')
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
+        SFTPClient.new('localhost', 'user1', 'pass')
       end
     end
 
-    describe 'file utilities' do
-      let(:file_1) {double("sftp_dir", :name => ".")}
-      let(:dir_1) {double("sftp_dir", :name => "import_docs")}
-      let(:dir_2) {double("sftp_dir", :name => "training_docs")}
+    context 'file utilities' do
+      let(:sftp_cli) { SFTPClient.new('localhost', 'user1', 'pass') }
+      let(:test_file) { File.expand_path('../../support/test1.txt', __FILE__) }
+      let(:file_1) { double('sftp_dir', name: '.') }
+      let(:dir_1) { double('sftp_dir', name: 'import_docs') }
+      let(:dir_2) { double('sftp_dir', name: 'training_docs') }
 
-      it "should return entries without prepended '.' " do
-        sftp_mock.stub_chain(:dir, :entries).with(anything()).and_return([file_1, dir_1, dir_2])
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        expect(@sftp_cli.entries('Desktop')).to eq([dir_1.name, dir_2.name])
+      before do
+        allow(sftp_mock).to receive_message_chain(:file, :open).with(anything()).and_return(File.open(test_file))
       end
 
-      it "should return with all files including '.' " do
-        sftp_mock.stub_chain(:dir, :entries).with(anything()).and_return([file_1, dir_1, dir_2])
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        expect(@sftp_cli.entries('Desktop', true)).to eq([file_1.name, dir_1.name, dir_2.name])
+      describe '#entries' do
+        before do
+          allow(sftp_mock).to receive_message_chain(:dir, :entries).with('Desktop').and_return([file_1, dir_1, dir_2])
+        end
+
+        it "should return entries without prepended '.' " do
+          expect(sftp_cli.entries('Desktop')).to eq([dir_1.name, dir_2.name])
+        end
+
+        it "should return with all files including '.' " do
+          expect(sftp_cli.entries('Desktop', true)).to eq([file_1.name, dir_1.name, dir_2.name])
+        end
       end
 
-      it 'should read both lines from test file' do
-        file = File.expand_path('../../support/test1.txt', __FILE__)
-        sftp_mock.stub_chain(:file, :open).with(anything()).and_return(File.open(file))
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        expect(@sftp_cli).to receive(:puts).twice
-        @sftp_cli.read(file)
+      describe '#read' do
+        it 'should read both lines from test file' do
+          expect(sftp_cli).to receive(:puts).twice
+          sftp_cli.read(test_file)
+        end
       end
 
-      it 'should get the size from test file' do
-        file = File.expand_path('../../support/test1.txt', __FILE__)
-        sftp_mock.stub_chain(:file, :open).with(anything()).and_return(File.open(file))
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        expect(@sftp_cli.size(file)).to eq(File.size(file))
+      describe '#size' do
+        it 'should get the size from test file' do
+          expect(sftp_cli.size(test_file)).to eq(File.size(test_file))
+        end
       end
 
-      it 'should get the atime from test file' do
-        file = File.expand_path('../../support/test1.txt', __FILE__)
-        sftp_mock.stub_chain(:file, :open).with(anything()).and_return(File.open(file))
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        expect(@sftp_cli.atime(file)).to eq(File.atime(file))
+      describe '#atime' do
+        it 'should get the atime from test file' do
+          expect(sftp_cli.atime(test_file)).to eq(File.atime(test_file))
+        end
       end
 
-      it 'should get the mtime from test file' do
-        file = File.expand_path('../../support/test1.txt', __FILE__)
-        sftp_mock.stub_chain(:file, :open).with(anything()).and_return(File.open(file))
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        expect(@sftp_cli.mtime(file)).to eq(File.mtime(file))
+      describe '#mtime' do
+        it 'should get the mtime from test file' do
+          expect(sftp_cli.mtime(test_file)).to eq(File.mtime(test_file))
+        end
       end
 
-      it 'should return false for directory not found' do
-        sftp_response = Object.new
-        def sftp_response.code; code = 2 end
-        def sftp_response.message; message = 'no such file' end
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        @sftp_cli.stub(:entries).and_raise(Net::SFTP::StatusException.new(sftp_response))
-        expect(@sftp_cli.has_directory?('bogus_directory')).to eql(false)
+      describe '#has_directory?' do
+        it 'should return false for directory not found' do
+          sftp_response = Object.new
+          def sftp_response.code; code = 2 end
+          def sftp_response.message; message = 'no such file' end
+          allow(sftp_mock).to receive_message_chain(:dir, :entries, :any?).and_raise(Net::SFTP::StatusException.new(sftp_response))
+          expect(sftp_cli.has_directory?('bogus_directory')).to eql(false)
+        end
       end
 
-      it 'should change directory source path' do
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        @sftp_cli.stub(:has_directory?).with(anything()).and_return(true)
-        @sftp_cli.chdir('projects')
-        expect(@sftp_cli.source).to eql('./projects')
+      describe '#chdir' do
+        before do
+          allow(sftp_cli).to receive(:has_directory?).with(anything()).and_return(true)
+        end
+
+        it 'should change directory source path' do
+          sftp_cli.chdir('projects')
+          expect(sftp_cli.source).to eq('projects')
+        end
+
+        it 'should sub tilda for period in path' do
+          sftp_cli.chdir('~/some_dir/working_dir')
+          expect(sftp_cli.source).to eq('./some_dir/working_dir')
+        end
       end
 
-      it 'should sub tilda in path' do
-        @sftp_cli = SFTPClient.new('localhost', 'user1', 'pass')
-        @sftp_cli.stub(:has_directory?).with(anything()).and_return(true)
-        @sftp_cli.chdir('~/some_dir/working_dir')
-        expect(@sftp_cli.source).to eql('./some_dir/working_dir')
+      describe '#reset_path!' do
+        it 'resets source' do
+          sftp_cli.source = 'some_dir/one'
+          sftp_cli.reset_path!
+          expect(sftp_cli.source).to eq('.')
+        end
       end
     end
 
